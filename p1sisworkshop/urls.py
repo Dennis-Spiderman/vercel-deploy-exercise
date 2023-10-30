03b8268cb187c21f40f30f4bae2094f4d0eb4862
Dennis Masso <dennismassomacias@gmail.com>
2023-10-30 17:05:09 -0500

    aç

--- p1sisworkshop/urls.py
+++ p1sisworkshop/urls.py
@@ -35,3 +35,6 @@ urlpatterns = [
     path('signin/', Signin.as_view(), name="signin"),
     path('signout/', CoursesMain.signout, name="signout"),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
